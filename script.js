0f0f334dacc8401008c09cfc5f4013e523985647
Luiz Fernando Forte <LFFORTE@GMAIL.COM>
2024-02-02 20:27:28 -0300

    Exercícios DOM-

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 const frutas = ["laranja", "limão", "uva"]
+
+document.getElementById('fruta-1').innerHTML = frutas[0];
+document.getElementById('fruta-2').innerHTML = frutas[1];
+document.getElementById('fruta-3').innerHTML = frutas[2];
+
+const add_fruta = () => {
+
+  document.getElementById('fruta-4').innerHTML = document.getElementById('fruta4').value;
+  document.getElementById('fruta4').value = "";
+
+}
